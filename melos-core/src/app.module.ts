c49b7059ca96bb5cd19741e9a0873f3de74f726d
thanhhuy5902 <tranthanhhuy050902@gmail.com>
2025-02-19 22:17:17 +0700

    [ADDED] create category and get category by id

--- melos-core/src/app.module.ts
+++ melos-core/src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SongsModule } from './songs/songs.module';
+import { AuthModule } from './auth/auth.module';
+import { SupabaseModule } from './supabase/supabase.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SongsModule,
+    AuthModule,
+    SupabaseModule,
+    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
