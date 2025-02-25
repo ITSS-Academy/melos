c7f3f8a18aa58e49f11fc7ced737c2073dd24afe
MINHHUY24 <Huy.lm04266@sinhvien.hoasen.edu.vn>
2025-02-25 14:11:58 +0700

    pull new code

--- melos/src/app/page/category/category.component.ts
+++ melos/src/app/page/category/category.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MaterialModule} from '../../shared/material.module';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
