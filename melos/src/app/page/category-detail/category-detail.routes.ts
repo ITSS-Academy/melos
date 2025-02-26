7c52a22dc3e55a8a1ef0a89b08059254283707c2
thanhhuy5902 <tranthanhhuy050902@gmail.com>
2025-02-26 09:51:43 +0700

    [FIXED] routing search

--- melos/src/app/page/category-detail/category-detail.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CategoryDetailComponent } from './category-detail.component';
-
-export const CATEGORY_DETAIL_ROUTES: Routes = [
-  {
-    path: '',
-    component: CategoryDetailComponent,
-  },
-];
