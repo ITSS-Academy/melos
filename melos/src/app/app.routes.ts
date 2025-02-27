badf287a5538cb09727930c8aa0389a1f283a7a5
thien nguyen <linhdo4001@gmail.com>
2025-02-27 16:53:01 +0700

    feat: categoty and categoly-detail, playlist

--- melos/src/app/app.routes.ts
+++ melos/src/app/app.routes.ts
@@ -14,6 +14,13 @@ export const routes: Routes = [
       ),
   },
   {
+    path: 'category-detail/:id',
+    loadChildren: () =>
+      import('../app/page/category-detail/category-detail.routes').then(
+        (m) => m.CATEGORY_DETAIL,
+      ),
+  },
+  {
     path: 'profile',
     loadChildren: () =>
       import('../app/page/profile/profile.routes').then(
@@ -30,6 +37,12 @@ export const routes: Routes = [
     loadChildren: () =>
       import('../app/page/search/search.routes').then((m) => m.SEARCH_ROUTES),
   },
+  {
+    path: 'playlist',
+    loadChildren: () =>
+      import('./page/playlist/playlist.routes').then((m) =>m.PLAYLIST_ROUTES),
+  },
+
 
   {
     path: '',
