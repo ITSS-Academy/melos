556dd9e94ccc00a11b954c8569800b39519cf9e7
thanhhuy5902 <tranthanhhuy050902@gmail.com>
2025-02-27 17:31:15 +0700

    [FIXED] fix comoponent

--- melos/src/app/page/playlist/playlist.component.ts
+++ melos/src/app/page/playlist/playlist.component.ts
@@ -1,42 +1,38 @@
 import { Component } from '@angular/core';
-import {PlaylistCardComponent} from '../../components/playlist-card/playlist-card.component';
+import { PlaylistCardComponent } from '../../shared/components/playlist-card/playlist-card.component';
 
 @Component({
   selector: 'app-playlist',
   standalone: true,
-  imports: [
-    PlaylistCardComponent
-  ],
+  imports: [PlaylistCardComponent],
   templateUrl: './playlist.component.html',
-  styleUrl: './playlist.component.scss'
+  styleUrl: './playlist.component.scss',
 })
 export class PlaylistComponent {
-
   playlists = [
     {
-      name: "Radio",
-      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/5/5/6/8/5568d11517ab8e384132d7f1c9e9434e.jpg",
-      comment:"",
-      tag:"",
-      category:"Anison",
-      singer_name:""
+      name: 'Radio',
+      img: 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/5/5/6/8/5568d11517ab8e384132d7f1c9e9434e.jpg',
+      comment: '',
+      tag: '',
+      category: 'Anison',
+      singer_name: '',
     },
     {
-      name: "Taylor Swift",
-      img: "https://upload.wikimedia.org/wikipedia/en/1/1f/Taylor_Swift_-_Taylor_Swift.png",
-      comment:"",
-      tag:"",
-      category:"Country music",
-      singer_name:"Taylor Swift"
+      name: 'Taylor Swift',
+      img: 'https://upload.wikimedia.org/wikipedia/en/1/1f/Taylor_Swift_-_Taylor_Swift.png',
+      comment: '',
+      tag: '',
+      category: 'Country music',
+      singer_name: 'Taylor Swift',
     },
     {
-      name: "Your playlist",
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQat97yEF4kAwu3VAg-y8CxpP4GUGAvZEIMOye1RQ7ICXElVlz_drAm8_xga1fNXHvr0GA&usqp=CAU",
-      comment:"",
-      tag:"",
-      category:"Country music",
-      singer_name:"Taylor Swift"
+      name: 'Your playlist',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQat97yEF4kAwu3VAg-y8CxpP4GUGAvZEIMOye1RQ7ICXElVlz_drAm8_xga1fNXHvr0GA&usqp=CAU',
+      comment: '',
+      tag: '',
+      category: 'Country music',
+      singer_name: 'Taylor Swift',
     },
-
-  ]
+  ];
 }
---
+++ melos/src/app/shared/components/category-card/category-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-category-card',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './category-card.component.html',
+  styleUrl: './category-card.component.scss',
+})
+export class CategoryCardComponent {
+  @Input() id!: number;
+  @Input() img!: string;
+  @Input() comment!: string;
+  @Input() tag!: string;
+  @Input() category: string = '';
+}
