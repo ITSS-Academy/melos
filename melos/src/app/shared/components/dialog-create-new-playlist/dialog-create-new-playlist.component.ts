03505c2e86c06c28219bc1c03bd9d734c68cfe12
thien nguyen <linhdo4001@gmail.com>
2025-03-04 13:54:04 +0700

    [added] create playlist

--- melos/src/app/shared/components/dialog-create-new-playlist/dialog-create-new-playlist.component.ts
+++ melos/src/app/shared/components/dialog-create-new-playlist/dialog-create-new-playlist.component.ts
@@ -1,31 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MaterialModule} from '../../material.module';
 import {MatDialogRef} from '@angular/material/dialog';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {FormGroup, FormControl} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
+
+interface Playlist {
+  img : string
+  name: string
+  description: string
+}
 @Component({
   selector: 'app-dialog-create-new-playlist',
   standalone: true,
-  imports: [MaterialModule, ReactiveFormsModule],
+  imports: [MaterialModule, ReactiveFormsModule, NgIf],
   templateUrl: './dialog-create-new-playlist.component.html',
   styleUrl: './dialog-create-new-playlist.component.scss'
 })
-export class DialogCreateNewPlaylistComponent{
+export class DialogCreateNewPlaylistComponent implements OnInit{
   constructor(
     public dialogRef: MatDialogRef<DialogCreateNewPlaylistComponent>,
+    private fb : FormBuilder
 
   ) {}
+  form!: FormGroup;
+  result: string = ''
+  playlists: Playlist[] = []
+  imgPath: string = ''
 
-  closeDialog(): void {
-    this.dialogRef.close();
+  ngOnInit() {
+    this.form= this.fb.group({
+      img: ['', Validators.required],
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    })
   }
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl('')
-  });
 
-  onSubmit() {
-    console.log('Form Data:', this.myForm.value);
+  uploadImg(event: any) {
+    const file = event.target.files[0];
+    const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
+
+    if (file) {
+      const fileExtension = file.name.split('.').pop().toLowerCase();
+
+      if (!file.type.startsWith('image/') || !allowedExtensions.includes(fileExtension)) {
+        alert('Chỉ được upload file JPG, JPEG, PNG, GIF, hoặc WEBP!');
+        return;
+      }
+      this.imgPath = URL.createObjectURL(file); // Tạo URL ảnh tạm thời
+      this.form.patchValue({img: this.imgPath}); // Gán đường dẫn vào form
+    }
   }
+  createPlaylist(){
+    if( this.form.invalid){
+      this.result= 'Form is invalid'
+      return
+    }
+
+    const {img, name, description} = this.form.value
+
+    // them san pham
+    this.playlists.push({img, name, description})
+
+    console.log(this.playlists)
+  }
+
 }
