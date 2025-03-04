74e5751d4f0cf922fce208c6c4e245ca37607e32
thanhhuy5902 <tranthanhhuy050902@gmail.com>
2025-03-04 10:55:00 +0700

    [ADDED] Routing profile

--- melos/src/app/page/search/search.component.ts
+++ melos/src/app/page/search/search.component.ts
@@ -1,34 +1,37 @@
 import { Component } from '@angular/core';
-import {Router, RouterLink, RouterOutlet} from '@angular/router';
-import {MatTab, MatTabGroup} from '@angular/material/tabs';
-import {CardArtistComponent} from '../../shared/components/card-artist/card-artist.component';
-import {SearchAllComponent} from './components/search-all/search-all.component';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CardArtistComponent } from '../../shared/components/card-artist/card-artist.component';
+import { SearchAllComponent } from './components/search-all/search-all.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [RouterOutlet, MatTab, MatTabGroup, CardArtistComponent, RouterLink, SearchAllComponent],
+  imports: [
+    RouterOutlet,
+    MatTab,
+    MatTabGroup,
+    CardArtistComponent,
+    RouterLink,
+    SearchAllComponent,
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
-
   constructor(private router: Router) {}
-
 
   onTabChange(event: any) {
     switch (event.index) {
       case 0:
-        this.router.navigate(['/search/search-all']);
+        this.router.navigate(['/search/search-all']).then();
         break;
       case 1:
-        this.router.navigate(['/search/search-peoples']);
+        this.router.navigate(['/search/search-peoples']).then();
         break;
       case 2:
-        this.router.navigate(['/search/search-song']);
+        this.router.navigate(['/search/search-song']).then();
         break;
     }
   }
-
-
 }
