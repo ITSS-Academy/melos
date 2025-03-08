9ebb95e81a8c4f45004d1c5b9d1e2c8286c25b71
MINHHUY24 <Huy.lm04266@sinhvien.hoasen.edu.vn>
2025-03-08 16:08:56 +0700

    feat: new file

--- melos/src/app/shared/pipes/id-to-name.pipe.ts
+++ melos/src/app/shared/pipes/id-to-name.pipe.ts
@@ -10,12 +10,18 @@ import { AuthService } from '../../services/auth/auth.service';
 export class IdToNamePipe implements PipeTransform {
   constructor(private authService: AuthService) {}
 
-  transform(uid: string): Observable<string> {
-    return this.authService.getAuthByUid(uid).pipe(
-      map((auth: any) => {
-        const url = auth.name;
-        return url;
-      }),
-    );
+  transform(uid: string| null): Observable<string> {
+    if(uid) {
+      return this.authService.getAuthByUid(uid).pipe(
+        map((auth: any) => {
+          const url = auth.name;
+          return url;
+        }),
+      );
+    }else {
+      return new Observable((observer) => {
+        observer.next('');
+      });
+    }
   }
 }
